22b15c6ff6f6a5b9524cc0febf5fa6cd156f813e
cahilljpaul <cahilljpaul@googlemail.com>
2025-05-03 06:57:17 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Paul Cahill on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
